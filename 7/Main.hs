75397e27355ef004eda854f60ece1e48a082491d
John Cinnamond <john@cinnamond.me.uk>
2021-12-07 18:53:53 +0000

    Day 7
    Not much time today so I went with naive-and-a-bit-slow

---
+++ 7/Main.hs
@@ -0,0 +1,31 @@
+module Main (main) where
+
+nums :: String -> [Int]
+nums "" = []
+nums (',' : rest) = nums rest
+nums s =
+    let (v, rest) = break (== ',') s
+     in read v : nums rest
+
+simpleCost :: [Int] -> Int -> Int
+simpleCost [] _ = 0
+simpleCost (x : xs) y = abs (x - y) + simpleCost xs y
+
+costs :: ([Int] -> Int -> Int) -> [Int] -> [Int]
+costs cost xs = fmap (cost xs) [min .. max]
+  where
+    min = minimum xs
+    max = maximum xs
+
+solve :: [Int] -> Int
+solve = minimum . costs simpleCost
+
+sumCost :: [Int] -> Int -> Int
+sumCost [] _ = 0
+sumCost (x : xs) y = sum [1 .. (abs (x - y))] + sumCost xs y
+
+solve2 :: [Int] -> Int
+solve2 = minimum . costs sumCost
+
+main :: IO ()
+main = interact $ show . solve2 . nums
