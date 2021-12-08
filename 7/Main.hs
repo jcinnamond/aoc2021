593a9e28fe73f6307c3818db839762d5a75b2bcc
John Cinnamond <john@cinnamond.me.uk>
2021-12-08 17:44:54 +0000

    Try to make day 7 faster
    Starting time was 211 ms to solve part 2.
    
    I fixed the calculation of sumCost to avoid an additional addition.
    This reduced the time to 192ms.
    
    I rewrote the cost summing to keep track of the accumulating costs as
    it works through the list, and stop as soon as it exceeds a previous
    cheaper cost.
    This reduced the time to 73ms.

--- 7/Main.hs
+++ 7/Main.hs
@@ -4,8 +4,8 @@ nums :: String -> [Int]
 nums "" = []
 nums (',' : rest) = nums rest
 nums s =
-    let (v, rest) = break (== ',') s
-     in read v : nums rest
+  let (v, rest) = break (== ',') s
+   in read v : nums rest
 
 simpleCost :: [Int] -> Int -> Int
 simpleCost [] _ = 0
@@ -13,9 +13,9 @@ simpleCost (x : xs) y = abs (x - y) + simpleCost xs y
 
 costs :: ([Int] -> Int -> Int) -> [Int] -> [Int]
 costs cost xs = fmap (cost xs) [min .. max]
-  where
-    min = minimum xs
-    max = maximum xs
+ where
+  min = minimum xs
+  max = maximum xs
 
 solve :: [Int] -> Int
 solve = minimum . costs simpleCost
@@ -23,11 +23,40 @@ solve = minimum . costs simpleCost
 sumCost :: [Int] -> Int -> Int
 sumCost [] _ = 0
 sumCost (x : xs) y = c (abs (x - y)) + sumCost xs y
-  where
-    c n = (n * (n - 1)) `div` 2 + n
+ where
+  c n = (n * (n + 1)) `div` 2
 
-solve2 :: [Int] -> Int
-solve2 = minimum . costs sumCost
+lowestCost' ::
+  -- | The list of positions to sum up
+  [Int] ->
+  -- | The position to move to
+  Int ->
+  -- | The current cost
+  Int ->
+  -- | The lowest cost found so far
+  Maybe Int ->
+  -- | The new lowest cost
+  Int
+lowestCost' [] _ acc _ = acc
+lowestCost' (pos : ps) target acc lowest =
+  let diff = abs (pos - target)
+      cost = (diff * (diff + 1)) `div` 2
+      newAcc = acc + cost
+   in case lowest of
+        Just l | newAcc > l -> l
+        _ -> lowestCost' ps target newAcc lowest
+
+fasterCost' :: [Int] -> [Int] -> Maybe Int -> Maybe Int
+fasterCost' _ [] acc = acc
+fasterCost' positions (t : ts) lowest =
+  let l = lowestCost' positions t 0 lowest
+   in fasterCost' positions ts (Just l)
+
+fasterCost :: [Int] -> Maybe Int
+fasterCost positions = fasterCost' positions [min .. max] Nothing
+ where
+  min = minimum positions
+  max = maximum positions
 
 main :: IO ()
-main = interact $ show . solve2 . nums
+main = interact $ show . fasterCost . nums
